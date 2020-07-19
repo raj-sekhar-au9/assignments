22a342d73bafe63eaf06c71911f221a78583d848
Raj Sekhar <krishnasylvester@gmail.com>
2020-07-19 22:43:56 +0530

    Create week-02 day-1.py

---
+++ ass_week_2-3/week-02 day-1.py
@@ -0,0 +1,48 @@
+# Question 1: Play with repl while running commands and operators.
+
+a = 5
+b = 4
+
+c = a + b 
+
+c = a * b 
+c = a - b
+c = a / b
+
+
+# Question 2: Given a no print its remainder. Which command will you use ?
+
+number = 10,  
+remainder_of_number = 10%2
+
+# Question 3: Experiment with str concatenation.
+
+first_name = "raj"
+last_name = "selkhar"
+
+print(first_name + last_name)
+
+# or 
+
+print("raj" + "sekhar")
+
+# Question 4: try naming some variables and see what all variables names throws error .
+
+a = 10 
+# no error
+_12345 = "raj"
+# no error 
+Base = 200
+# no error 
+123_number = "assignment 1"
+# error 
+%python = "language"
+# error 
+
+# from the following result, its concluded that any variable starting with  a-z,A-Z or _ is considered as a variable and otherwise will give an error 
+
+# Question 5: Explore more on the things we learned in the class.
+# answer -  I learnt about if, elif and else statements
+
+
+.................................................the end........................................................................................................
