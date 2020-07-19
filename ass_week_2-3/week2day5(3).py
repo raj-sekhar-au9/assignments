b0c4d536bf047f670afc01b1d5492a9ed2177e7d
Raj Sekhar <krishnasylvester@gmail.com>
2020-07-19 23:45:21 +0530

    Create week2day5(3).py

---
+++ ass_week_2-3/week2day5(3).py
@@ -0,0 +1,12 @@
+# initializing string  
+test_str = "Geeks  forGeeks"
+  
+# printing original string  
+print("The original string is : " + test_str) 
+  
+# Using in operator 
+# Check for spaces  
+res = " " in test_str 
+  
+# printing result  
+print("Does string contain spaces ? " + str(res)) 
