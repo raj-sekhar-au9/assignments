f1bc330e0bc07115e8d6b346177d8a8cc5413611
Raj Sekhar <krishnasylvester@gmail.com>
2020-07-19 22:55:19 +0530

    Create 1stfile.py

---
+++ ass_week_2-3/1stfile.py
@@ -0,0 +1,15 @@
+#1. practice the commands given in the lecture
+1 ls ans-this command displays the list of files in the present directory
+2 ls -l ans-Here in ls command if we attach -l flag , instead of just showing the list of files in a
+3 directory it will also show additional details of the files such as file timestamp, file size, etc
+4 pwd ans-this command displays the location of current working directory
+5 touch ans-touch creates a file for us in the current directory with any required extension
+6 cd .. ans- just like how cd command will take you forward by one directory, cd .. command
+will take you back by one directory
+7 cd ans-this command is used to change the current directory to required directory
+8 cd ~ ans-This command will take you directly to the home directory irrespective of which ever
+directory you are in
+
+#22. Set up you system
+For windows install git bash for windows
+ans dne
