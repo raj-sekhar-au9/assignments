168a9aff98bd2ce511362a1f754fee8c9f6de2d2
Raj Sekhar <krishnasylvester@gmail.com>
2020-07-23 09:07:49 +0530

    Create week4day3(2).py

---
+++ codingchallenge/week4day3(2).py
@@ -0,0 +1,9 @@
+#path problem
+def numberOfPaths(p, q): 
+
+    dp = [1 for i in range(q)] 
+    for i in range(p - 1): 
+        for j in range(1, q): 
+            dp[j] += dp[j - 1] 
+    return dp[q - 1] 
+print(numberOfPaths(3, 3)
